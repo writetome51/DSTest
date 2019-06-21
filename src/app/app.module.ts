e851774cf9244b7d63b2c55bab13c6456939015b
Steve Thompson <writetome51@gmail.com>
2019-06-21 13:42:50 -0600

    improved DataSourceService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { DataSourceService } from './services/data-source.service';
+import { NgModule } from '@angular/core';
+import { PaginatorService } from './services/paginator.service';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [DataSourceService, PaginatorService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/services/data-source.service.ts
+++ src/app/services/data-source.service.ts
@@ -1,30 +1,35 @@
 import { Injectable } from '@angular/core';
+import { PublicArrayContainer } from '@writetome51/public-array-container';
+import { getTail } from '@writetome51/array-get-head-tail';
+import { getAdjacentAt } from '@writetome51/array-get-adjacent-at';
 
 
 @Injectable({
     providedIn: 'root'
 })
-export class DataSourceService {
-
-    private __dataTotal;
+export class DataSourceService extends PublicArrayContainer {
 
 
     constructor() {
+        super();
     }
 
 
     get dataTotal(): number {
-        return;
+        return this.data.length;
     }
 
 
     getBatch(batchNumber: number, itemsPerBatch: number, isLastBatch: boolean): any[] {
-        const start = (batchNumber - 1) * itemsPerBatch + 1;
-        let end = start + itemsPerBatch - 1;
+        const start = ((batchNumber - 1) * itemsPerBatch);
         if (isLastBatch) {
-            end = this.dataTotal;
-        }
 
-      //  return ?
+            // ...only return the remaining items in array, not itemsPerBatch:
+            let numItemsToGet = (this.data.length - start);
+            return getTail(numItemsToGet, this.data);
+        }
+        else {
+            return getAdjacentAt(start, itemsPerBatch, this.data);
+        }
     }
 }
