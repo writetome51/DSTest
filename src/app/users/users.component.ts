562c38397636a7dab4c60fdb23a479d9e12f6d81
Steve Thompson <writetome51@gmail.com>
2019-06-28 02:09:26 -0600

    minor changes

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.sass']
+  templateUrl: './users.component.html'
 })
 export class UsersComponent implements OnInit {
 
